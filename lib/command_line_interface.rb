827180808ef6f51b55cee7e85479b19f9134a6f3
Alex Merecka <merecka@gmail.com>
2019-04-30 03:44:01 +0000

    Added methods to command_line_interface.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,11 +6,23 @@ class CommandLineInterface
 
   def display_options
     puts "Welcome to the Daily Surf Forecast:"
-    puts "  Choose which surf spot report you would like to see by inputting the corresponding number."
+    puts "  Choose which surf spot report you would like to see by typing the corresponding number."
     puts "  1.  Playa Santana"
-    input = gets.chomp
+    choice_1 = gets.chomp
 
-    Scraper.scrape_magicseaweed_page(input)
+    case surf_spot
+    when choice_1 == 1
+      playa_santana
+    end
+  end
+
+  def playa_santana
+    puts "  Choose which option you would like to see by typing the corresponding number."
+    puts "  1.  Today's surf report"
+    puts "  2.  Surf spot description"
+    choice_2 = gets.chomp
+
+    Scraper.scrape_magicseaweed_page(choice_2)
   end
 
 end
