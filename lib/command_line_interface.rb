bff663fd13ec617e94de6965777c40aee342a26f
Alex Merecka <merecka@gmail.com>
2019-05-01 23:22:22 +0000

    Updated CLI.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -24,11 +24,16 @@ class CommandLineInterface
         puts "#{counter}.  #{key}"
         counter += 1
       end
+
       puts "Choose which surf region you would like to see by typing the corresponding number."
       choice = gets.chomp
-
-      region_to_scrape = region_list_hash[choice]
-      binding.pry
+      if choice.to_i >= 1 && choice.to_i <= scrape.cr_region_hash.length
+        region_to_scrape = region_list_hash[choice]
+        scrape.scrape_msw_region_surfspot_page(scrape.cr_region_hash[region_to_scrape])
+        binding.pry
+      else
+        puts "Please enter a number corresponding to the surf region you would like to see."
+      end
     end
   end
 
