ce99fc6704439ac882b9ed2904dcab86e6b920f6
Alex Merecka <merecka@gmail.com>
2019-05-01 01:35:13 +0000

    Worked on Magic Seaweed scraper method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,12 +12,13 @@ class Scraper
   @@msw_url = {1 => "https://magicseaweed.com/Playa-Santa-Teresa-Surf-Report/914/"}
 
   def scrape_magicseaweed_page(choice)
-    #binding.pry
     html = open(@@msw_url[choice])
     doc = Nokogiri::HTML(html)
-    binding.pry
-    doc.css(".hidden-xs heavy").text
-
+    puts doc.css(".forecast-sub-title-fluid")[0].text.strip + ":"
+    puts doc.css(".list-group-item .nomargin-bottom")[0].text
+    puts doc.css(".list-group-item-block:nth-child(1)").text
+    puts doc.css(".list-group-item-block+ .list-group-item-block").text
+    puts doc.css(".col-xs-6:nth-child(1)")[0].text
   end
 
   def scrape_surf_forecast_page(choice)
