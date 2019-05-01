6d7eac7e456bddf2a8fd65bfe7c43c0a9615a031
Alex Merecka <merecka@gmail.com>
2019-05-01 05:56:24 +0000

    Worked on scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,18 +7,22 @@ class Scraper
 
   def run
     scrape_magicseaweed_page(1)
+    # scrape_surf_forecast_page(1)
   end
 
   @@msw_url = {1 => "https://magicseaweed.com/Playa-Santa-Teresa-Surf-Report/914/"}
+
+  def scrape_msw_costarica_page(url)
 
   def scrape_magicseaweed_page(choice)
     html = open(@@msw_url[choice])
     doc = Nokogiri::HTML(html)
     puts doc.css(".forecast-sub-title-fluid")[0].text.strip + ":"
-    puts doc.css(".list-group-item .nomargin-bottom")[0].text
-    puts doc.css(".list-group-item-block:nth-child(1)").text
-    puts doc.css(".list-group-item-block+ .list-group-item-block").text
-    puts doc.css(".col-xs-6:nth-child(1)")[0].text
+    puts "Weather: " + doc.css(".list-group-item .nomargin-bottom")[0].text.strip
+    puts "Winds: " + doc.css(".h5.nomargin-top").text
+    puts doc.css(".list-group-item-block:nth-child(1)").text.strip
+    puts doc.css(".list-group-item-block+ .list-group-item-block").text.strip
+    puts "Tides:  " + doc.css(".col-xs-6:nth-child(1)")[0].text.strip
   end
 
   def scrape_surf_forecast_page(choice)
